7c015a6a77114289688bbccbfedaecbf9c2950b4
Rafail Abdyushev <rafaelab@mail.ru>
2023-07-09 09:02:26 +0300

    class fabric static method

--- FabricOfClasses/createObj.py
+++ FabricOfClasses/createObj.py
@@ -2,6 +2,8 @@ import Modules as m
 
 def main():
     myDog = m.Fabric.create(m.Dog, 'Sharik')
+    print(isinstance(myDog, m.Dog))
+    print(myDog)
     print(myDog.name)
 
 if __name__ == '__main__':
